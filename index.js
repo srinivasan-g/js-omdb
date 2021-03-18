b2fe3f58d502fc76cc2c29a22fcfd933794e9a36
srinivasan-g <mr.gsrinivasan@gmail.com>
2021-03-18 18:33:27 +0000

    2 files modified

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // Import stylesheets
-import './style.css';
-
+import "./style.css";
 // Write Javascript code!
-const appDiv = document.getElementById('app');
+const appDiv = document.getElementById("app");
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
