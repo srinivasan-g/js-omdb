86ada050ccca42b7ffcfd8e80151a95b5d24b476
srinivasan-g <mr.gsrinivasan@gmail.com>
2021-03-18 18:55:41 +0000

    2 files modified

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 // Import stylesheets
 import "./style.css";
 // Write Javascript code!
+pathToData = "chatTranscript.lines";
+console.log(">>> pathToData: ", pathToData);
 const appDiv = document.getElementById("app");
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+var updateCallback = function(data) {
+  // Do something with the returning data
+  var path = data.key;
+  var value = data.newValue;
+  console.log(path);
+  // called each time the value is updated.
+  // If there's an existing value when bind is called - this callback
+  // will be called with the existing value
+};
+
+var notifyWhenDone = function(err) {
+  if (err) {
+    // Do something with the error
+  }
+  // called when the bind is completed successfully,
+  // or when the action terminated with an error.
+};
+
+var pathToData = "visitorInfo.visitorName";
+
+lpTag.agentSDK.bind(pathToData, updateCallback, notifyWhenDone);
